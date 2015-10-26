13a094913d7a96c4cd051a319a5836d58eb237a5
ingduk2 <ingduk2@naver.com>
2015-10-26 09:28:28 +0900

    sql 파일 chart 추가

---
+++ w_semiProject_UI/WebContent/WEB-INF/semi.sql
@@ -0,0 +1,21 @@
+drop table chart;
+create table chart(
+a number,
+b number,
+c number,
+d number);
+select * from chart;
+insert into chart values(10,20,30,40);
+insert into chart values(20,30,40,50);
+insert into chart values(30,40,50,60);
+insert into chart values(40,50,60,70);
+insert into chart values(50,60,70,80);
+insert into chart values(60,70,80,90);
+insert into chart values(70,80,90,100);
+insert into chart values(80,90,100,110);
+insert into chart values(90,100,110,120);
+insert into chart values(100,110,120,130);
+insert into chart values(110,120,130,140);
+insert into chart values(120,130,140,150);
+
+commit;
